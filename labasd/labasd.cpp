55b5b26b9cd1f7542c31df05fd8a3b4e2a28be38
VornadRkk <126707014+VornadRkk@users.noreply.github.com>
2024-03-07 23:30:42 +0400

    add in main "rand"

--- labasd/labasd.cpp
+++ labasd/labasd.cpp
@@ -1,14 +1,67 @@
-﻿// labasd.cpp: определяет точку входа для приложения.
-//
-
+﻿#include <iostream>
 #include "labasd.h"
+#include <ctime>
+#include <time.h>
 
 using namespace std;
 
+int BinaryTree::count_completion = 0;
+int BinaryTree::count_search = 0;
+int BinaryTree::count_del = 0;
 int main()
 {
-	BinaryTree A(5);
-	A.insert(4);
-	A.erase(4);
-	A.print();
+	srand(time(NULL));
+	/*BinaryTree test(3);
+	test.insert(4);
+	test.insert(2);*/
+	BinaryTree A(500);
+	BinaryTree B(600);
+	BinaryTree C(700);
+	int size_begin = 1000;
+	int size_medium = 10000;
+	int size_end = 100000;
+	int n_time = 100;
+	int n_time_search = 1000;
+	for (int j = 0; j < n_time; j++) {
+		for (int i = 0; i < size_begin; ++i) {
+			A.insert(rand() % size_begin);
+		}
+	}
+	cout << A.get_count_completion()/ n_time << endl;
+	for (int j = 0; j < n_time; j++) {
+		for (int i = 0; i < size_medium; ++i) {
+			B.insert(rand() % size_medium);
+		}
+	}
+	cout << B.get_count_completion() / n_time << endl;
+	for (int j = 0; j < n_time; j++) {
+		for (int i = 0; i < size_end; ++i) {
+			C.insert(rand() % size_end);
+		}
+	}
+	cout << C.get_count_completion() / n_time << endl;
+	for (int j = 0; j < n_time_search; j++) {
+			A.Findelem(rand() % size_begin);
+	}
+	cout << A.get_count_search() / n_time_search << endl;
+	for (int j = 0; j < n_time_search; j++) {
+			B.Findelem(rand() % size_medium);
+	}
+	cout << B.get_count_search() / n_time_search << endl;
+	for (int j = 0; j < n_time_search; j++) {
+			C.Findelem(rand() % size_end);
+	}
+	cout << C.get_count_search() / n_time_search << endl;
+	for (int j = 0; j < n_time_search; j++) {
+		A.erase(rand() % size_begin);
+	}
+	cout << A.get_count_del() / n_time_search << endl;
+	for (int j = 0; j < n_time_search; j++) {
+		B.erase(rand() % size_medium);
+	}
+	cout << B.get_count_del() / n_time_search << endl;
+	for (int j = 0; j < n_time_search; j++) {
+		C.erase(rand() % size_end);
+	}
+	cout << C.get_count_del() / n_time_search << endl;
 }
--- labasd/labasd.h
+++ labasd/labasd.h
@@ -1,12 +1,17 @@
 ﻿#include <iostream>
 using namespace std;
 
+
 class BinaryTree {
 private:
+    static int count_completion;
+    static int count_search;
+    static int count_del;
     struct Node {
         int key;
         Node* left;
         Node* right;
+        Node() : key(0), left(nullptr), right(nullptr) {}
         Node(int _key) : key(_key), left(nullptr), right(nullptr) {}
         ~Node() {
             delete left;
@@ -20,17 +25,21 @@ private:
         bool insert(int _key) {
             if (_key < key) {
                 if (left == nullptr) {
+                    count_completion++;
                     left = new Node(_key);
                 }
                 else {
+                    count_completion++;
                     return left->insert(_key);
                 }
             }
             else if (_key > key) {
                 if (right == nullptr) {
+                    count_completion++;
                     right = new Node(_key);
                 }
                 else {
+                    count_completion++;
                     return right->insert(_key);
                 }
             }
@@ -40,6 +49,27 @@ private:
             if (_key == key) return true;
             if (_key < key && left) return left->contains(_key);
             if (_key > key && right) return right->contains(_key);
+            return false;
+        }
+        bool Findelem(int _key) {
+            if (_key == key) {
+                count_search++;
+                return true;
+            }
+            if (_key < key) {
+                count_search++; 
+                if (left != nullptr) {
+                    count_search++;
+                    return left->Findelem(_key);
+                }  
+            }
+            else {
+                count_search;
+                if (right != nullptr) {
+                    count_search++;
+                    return right->Findelem(_key);
+                }
+            }
             return false;
         }
     };
@@ -57,25 +87,31 @@ private:
     void erase(Node*& node, int _key) {
         if (node == nullptr) return;
         if (_key < node->key) {
+            count_del++;
             erase(node->left, _key);
         }
         else if (_key > node->key) {
+            count_del++;
             erase(node->right, _key);
         }
         else {
+            count_del++;
             if (node->left == nullptr) {
+                count_del++;
                 Node* temp = node->right;
                 node->right = nullptr;
                 delete node;
                 node = temp;
             }
             else if (node->right == nullptr) {
+                count_del++;
                 Node* temp = node->left;
                 node->left = nullptr;
                 delete node;
                 node = temp;
             }
             else {
+                count_del++;
                 Node* minRight = node->right;
                 while (minRight->left != nullptr) {
                     minRight = minRight->left;
@@ -87,6 +123,18 @@ private:
     }
 
 public:
+    bool Findelem(int _key) {
+        return root->Findelem(_key);
+    }
+    int get_count_completion() {
+        return count_completion;
+    }
+    int get_count_search() {
+        return count_search;
+    }
+    int get_count_del() {
+        return count_del;
+    }
     BinaryTree(int key) : root(new Node(key)) {}
     ~BinaryTree() {
         delete root;
